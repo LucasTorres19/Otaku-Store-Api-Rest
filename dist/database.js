7d07f1892658fce943f206c56f140a81a0fefddb
Lucas Torres <47805417+LucasTorres19@users.noreply.github.com>
2021-08-04 21:07:57 -0300

    Agregando la env var

--- dist/database.js
+++ dist/database.js
@@ -3,7 +3,7 @@
 var mongoose = require('mongoose'); // Connection to DB
 
 
-mongoose.connect('mongodb://wachiraton11:soyhermosho11@cluster0-shard-00-00.skuwb.mongodb.net:27017,cluster0-shard-00-01.skuwb.mongodb.net:27017,cluster0-shard-00-02.skuwb.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-7twvaf-shard-0&authSource=admin&retryWrites=true&w=majority', {
+mongoose.connect(process.env.DB_URL, {
   useUnifiedTopology: true,
   useNewUrlParser: true
 }, function (err, res) {
